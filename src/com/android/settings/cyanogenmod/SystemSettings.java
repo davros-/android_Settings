d68075a688460dfcad2ee69045c23c8361e9d8c5
Chris Phelps <cphelps76@cphelps76-HP-Pavilion-dv7.(none)>
2013-02-09 11:09:10 -0600

    changes for custom carrier label, working but currently fc's when OK in dialog is selected, to be fixed
    Conflicts:
    
    	res/xml/system_settings.xml

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -16,11 +16,15 @@
 
 package com.android.settings.cyanogenmod;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.view.IWindowManager;
@@ -36,8 +40,12 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.text.Spannable;
 import android.util.Log;
 import android.view.IWindowManager;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -56,6 +64,13 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mPowerButtonTorch;
     private static final String KEY_CHRONUS = "chronus";
     private static final String PREF_FORCE_DUAL_PANEL = "force_dualpanel";
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+    Preference mCustomLabel;
+
+    Context mContext;
+
+    String mCustomLabelText = null;
 
     CheckBoxPreference mDualpane;
     private boolean torchSupported() {
@@ -65,8 +80,11 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.system_settings);
+
+        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
 
         // Dont display the lock clock preference if its not installed
         removePreferenceIfPackageNotInstalled(findPreference(KEY_CHRONUS));
@@ -102,10 +120,41 @@ public class SystemSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), Settings.System.POWER_BUTTON_TORCH,
                     enabled ? 1 : 0);
             return true;
-        } else {
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        } else if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+
+            alert.setView(input);
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+ 
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.android.settings.LABEL_CHANGED");
+                    mContext.sendBroadcast(i);
+                }
+            });
+
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();   
         }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
+
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver cr = getActivity().getContentResolver();
 
@@ -135,5 +184,15 @@ public class SystemSettings extends SettingsPreferenceFragment implements
         }
         return false;
     }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
+    }   
 
 }
