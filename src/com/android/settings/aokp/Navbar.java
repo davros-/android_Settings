91c6ce2ed2146e5eb4d6a91099cc1ce6c84d9bea
davros <davros@droidconcepts.com>
2013-02-13 19:19:53 -0500

    Revert "Port ROMControl: Allow Custom Icon Tinting Option (2/2)"
    This reverts commit 1a12559e9919dcf79c7bb80a7d682fa099d5cd98.

--- src/com/android/settings/aokp/Navbar.java
+++ src/com/android/settings/aokp/Navbar.java
@@ -70,7 +70,6 @@ public class Navbar extends SettingsPreferenceFragment implements
     private static final String PREF_NAVBAR_MENU_DISPLAY = "navbar_menu_display";
     private static final String NAVIGATION_BAR_COLOR = "nav_bar_color";
     private static final String PREF_NAV_COLOR = "nav_button_color";
-    private static final String NAVIGATION_BAR_ALLCOLOR = "navigation_bar_allcolor";
     private static final String PREF_NAV_GLOW_COLOR = "nav_button_glow_color";
     private static final String PREF_GLOW_TIMES = "glow_times";
     private static final String PREF_NAVBAR_QTY = "navbar_qty";
@@ -96,7 +95,6 @@ public class Navbar extends SettingsPreferenceFragment implements
     // move these later
     ColorPickerPreference mNavigationColor;
     ColorPickerPreference mNavigationBarColor;
-    CheckBoxPreference mColorizeAllIcons;
     ColorPickerPreference mNavigationBarGlowColor;
     ListPreference mGlowTimes;
     ListPreference menuDisplayLocation;
@@ -180,10 +178,6 @@ public class Navbar extends SettingsPreferenceFragment implements
 
         mNavigationBarColor = (ColorPickerPreference) findPreference(PREF_NAV_COLOR);
         mNavigationBarColor.setOnPreferenceChangeListener(this);
-
-        mColorizeAllIcons = (CheckBoxPreference) findPreference("navigation_bar_allcolor");
-        mColorizeAllIcons.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
-                Settings.System.NAVIGATION_BAR_ALLCOLOR, false));
 
         mNavigationBarGlowColor = (ColorPickerPreference) findPreference(PREF_NAV_GLOW_COLOR);
         mNavigationBarGlowColor.setOnPreferenceChangeListener(this);
@@ -280,11 +274,6 @@ public class Navbar extends SettingsPreferenceFragment implements
                     Settings.System.NAVIGATION_BAR_SHOW,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             Helpers.restartSystemUI();
-            return true;
-        } else if (preference == mColorizeAllIcons) {
-            Settings.System.putBoolean(getActivity().getContentResolver(),
-                    Settings.System.NAVIGATION_BAR_ALLCOLOR,
-                    ((CheckBoxPreference) preference).isChecked() ? true : false);
             return true;
         } else if (preference == mEnableNavringLong) {
 
