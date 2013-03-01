c246282d4c2cae54effbf797ac7e75424ce3eb8f
Bajee11 <bajee11@gmail.com>
2013-03-01 08:03:27 -0500

    QuietHours Toggle (2/2)
    Required for longpress action.
    
    Reflect change in Settings.
    
    Signed-off-by: Bajee11 <bajee11@gmail.com>
    Change-Id: I2082e12fb2ce9e4063d960df540259eb6822fbcd
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/cyanogenmod/QuietHours.java

--- src/com/android/settings/cyanogenmod/QuietHours.java
+++ src/com/android/settings/cyanogenmod/QuietHours.java
@@ -17,7 +17,10 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
@@ -46,6 +49,10 @@ public class QuietHours extends SettingsPreferenceFragment implements
     private CheckBoxPreference mQuietHoursHaptic;
     private TimeRangePreference mQuietHoursTimeRange;
 
+    protected Handler mHandler;
+    private SettingsObserver mSettingsObserver;
+    private Context mContext;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +60,8 @@ public class QuietHours extends SettingsPreferenceFragment implements
         if (getPreferenceManager() != null) {
             addPreferencesFromResource(R.xml.quiet_hours_settings);
 
-            ContentResolver resolver = getActivity().getApplicationContext().getContentResolver();
-
+            mContext = getActivity().getApplicationContext();
+            ContentResolver resolver = mContext.getContentResolver();
             PreferenceScreen prefSet = getPreferenceScreen();
 
             // Load the preferences
@@ -70,9 +77,9 @@ public class QuietHours extends SettingsPreferenceFragment implements
             if (mQuietHoursNote != null && getResources().getBoolean(com.android.internal.R.bool.config_voice_capable) == false) {
                 getPreferenceScreen().removePreference(mQuietHoursNote);
             }
+            mSettingsObserver = new SettingsObserver(new Handler());
 
             // Set the preference state and listeners where applicable
-            mQuietHoursEnabled.setChecked(Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_ENABLED, 0) == 1);
             mQuietHoursTimeRange.setTimeRange(Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_START, 0),
                     Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_END, 0));
             mQuietHoursTimeRange.setOnPreferenceChangeListener(this);
@@ -127,5 +134,31 @@ public class QuietHours extends SettingsPreferenceFragment implements
             return true;
         }
         return false;
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+            observe();
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QUIET_HOURS_ENABLED), false,
+                    this);
+            updateSettings();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    protected void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+        mQuietHoursEnabled.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_ENABLED, 0) == 1);
     }
 }
