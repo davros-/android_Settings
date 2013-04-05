9b349af48a4b69914f12dff36fed130a8995e751
cphelps76 <cphelps76@gmail.com>
2013-04-05 00:10:52 -0500

    add about rom preference screen {x}...clickable link to twitter {x}...clickable link to github source {x}...clickable link to website {x}...paypal donate button
    Conflicts:
    
    	res/drawable-xhdpi/ic_settings_system.png

---
+++ src/com/android/settings/demented/About.java
@@ -0,0 +1,44 @@
+package com.android.settings.demented;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mIrcUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.prefs_about);
+        mSiteUrl = findPreference("dc_website");
+        mSourceUrl = findPreference("demented_source");
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://www.droidconcepts.com/");
+        } else if (preference == mSourceUrl) {
+            launchUrl("https://github.com/cphelps76");
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
